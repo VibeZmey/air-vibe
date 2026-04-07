02f458c9d1e7f39a243b39fa5f8c00994876937c
Zmeev Ilya <zmeev.i.v@edu.mirea.ru>
2026-04-07 16:39:48 +0300

    add create booking command

---
+++ backend/Flights/Application/Features/Bookings/CreateBooking/CreateBookingCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Flights.Application.Features.Bookings.CreateBooking;
+
+public class CreateBookingCommand : IRequest<Unit>
+{
+    public Guid UserId { get; private set; }
+    public Guid PassengerId { get; private set; }
+    public string SeatNumber { get; private set; }
+    public Guid FlightId { get; private set; }
+    public bool HasLuggage { get; private set; } = false;
+    public bool HasFood { get; private set; } = false;
+    public bool IsBusiness { get; private set; } = false; 
+}
---
+++ backend/Flights/Application/Features/Bookings/CreateBooking/CreateBookingHandler.cs
@@ -0,0 +1,50 @@
+﻿using Flights.Domain.Interfaces;
+using Flights.Domain.Models;
+using MediatR;
+
+namespace Flights.Application.Features.Bookings.CreateBooking;
+
+public class CreateBookingHandler : IRequestHandler<CreateBookingCommand, Unit>
+{
+    private readonly IFlightRepository _flightRepo;
+    private readonly IPassengerRepository _passengerRepo;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IBookingRepository _bookingRepo;
+
+    public CreateBookingHandler(IFlightRepository flightRepository,
+        IPassengerRepository passengerRepository,
+        IUnitOfWork unitOfWork,
+        IBookingRepository bookingRepository)
+    {
+        _flightRepo = flightRepository;
+        _passengerRepo = passengerRepository;
+        _unitOfWork = unitOfWork;
+        _bookingRepo = bookingRepository;
+    }
+    
+    public async Task<Unit> Handle(CreateBookingCommand request, CancellationToken cancellationToken)
+    {
+        var flight = await _flightRepo
+            .GetByIdAsync(request.FlightId, cancellationToken);
+        
+        var passenger = await _passengerRepo
+            .GetByIdAsync(request.PassengerId, cancellationToken);
+        
+        var booking = Booking.Create(
+            request.UserId, 
+            passenger, 
+            request.SeatNumber, 
+            flight);
+        
+        if(request.IsBusiness) booking
+            .UpgradeToBusiness(flight.BusinessPrice, passenger.Type);
+        
+        if(request.HasFood) booking.AddFood(flight.FoodPrice);
+        
+        if(request.HasLuggage) booking.AddLuggage(flight.LuggagePrice);
+        //TODO: сделать оплату тут где то хз
+        await _bookingRepo.AddAsync(booking, cancellationToken);
+        await _unitOfWork.SaveAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
