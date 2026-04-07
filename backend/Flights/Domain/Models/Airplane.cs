ceff68b736c734cd6270bcff3d5064adc31ed49a
Zmeev Ilya <zmeev.i.v@edu.mirea.ru>
2026-04-07 16:37:03 +0300

    update airplane model

---
+++ backend/Flights/Domain/Models/Airplane.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Flights.Domain.Models;
+
+public class Airplane
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; } 
+    public int Rows { get; set; }
+    public int Columns { get; set; }
+    
+    public int BuisnessRows { get; set; }
+    public int BuisnessColumns { get; set; }
+    public int TotalSeats { get; set; }
+    public int SpacePlusRow { get; set; }
+    
+    public int AirlineId { get; set; }
+    public Airline Airline { get; set; }
+    
+    public ICollection<Flight> Flights { get; set; }
+}
---
+++ backend/Flights/Infrastructure/Persistence/Configurations/AirplaneConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Flights.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Flights.Infrastructure.Persistence.Configurations;
+
+public class AirplaneConfiguration : IEntityTypeConfiguration<Airplane>
+{
+    public void Configure(EntityTypeBuilder<Airplane> builder)
+    {
+        builder.HasKey(a => a.Id);
+        
+        builder
+            .HasOne(a => a.Airline)
+            .WithMany(a => a.Airplanes)
+            .HasForeignKey(a => a.AirlineId);
+    }
+}
