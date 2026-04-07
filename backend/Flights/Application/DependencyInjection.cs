ca26a08e1b0dcd78d1553345573cf87bed5c6d93
Zmeev Ilya <zmeev.i.v@edu.mirea.ru>
2026-04-07 17:08:27 +0300

    add DI

---
+++ backend/Flights/Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Flights.Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+
+namespace Flights.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        return services;
+    }
+}
---
+++ backend/Flights/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Flights.Domain.Interfaces;
+using Flights.Infrastructure.Persistence;
+using Flights.Infrastructure.Repositories;
+using Flights.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flights.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<FlightsDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        services.AddScoped<IDocumentRepository, DocumentRepository>();
+        services.AddScoped<IPassengerRepository, PassengerRepository>();
+        services.AddScoped<IEncryptionService, EncryptionService>();
+        services.AddScoped<IFlightRepository, FlightRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        return services;
+    }
+}
