f8fa5d514a9c8c670eb26e87c1a2d26db60702f3
Zmeev Ilya <zmeev.i.v@edu.mirea.ru>
2026-04-07 16:39:29 +0300

    add confirm booking command

---
+++ backend/Flights/Application/Features/Bookings/ConfirmBooking/ConfirmBookingCommand.cs
@@ -0,0 +1,9 @@
+﻿using Flights.Domain.Models;
+using MediatR;
+
+namespace Flights.Application.Features.Bookings.ConfirmBooking;
+
+public class ConfirmBookingCommand : IRequest<Booking>
+{
+    public Guid BookingId { get; set; }
+}
---
+++ backend/Flights/Application/Features/Bookings/ConfirmBooking/ConfirmBookingHandler.cs
@@ -0,0 +1,37 @@
+﻿using Flights.Domain.Interfaces;
+using Flights.Domain.Models;
+using MediatR;
+
+namespace Flights.Application.Features.Bookings.ConfirmBooking;
+
+public class ConfirmBookingHandler : IRequestHandler<ConfirmBookingCommand, Booking>
+{
+    private readonly IBookingRepository _bookingRepo;
+    private readonly IFlightRepository _flightRepo;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ConfirmBookingHandler(IBookingRepository bookingRepository,
+        IFlightRepository flightRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _bookingRepo = bookingRepository;
+        _flightRepo = flightRepository;
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task<Booking> Handle(ConfirmBookingCommand request, CancellationToken cancellationToken)
+    {
+        var booking = await _bookingRepo.GetByIdAsync(request.BookingId, cancellationToken);
+        if (booking is null)
+            throw new ApplicationException("Booking not found");
+        
+        var flight = await _flightRepo.GetByIdAsync(booking.FlightId, cancellationToken);
+        if (flight is null)
+            throw new ApplicationException("Flight not found");
+        
+        booking.ConfirmPayment();
+        await _bookingRepo.AddAsync(booking, cancellationToken);
+        await _unitOfWork.SaveAsync(cancellationToken);
+        return booking;
+    }
+}
