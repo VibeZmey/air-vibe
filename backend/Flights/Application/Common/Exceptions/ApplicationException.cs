1fecf2f243ab7ba8b15b1f53d1a2ab63f023c1e2
Zmeev Ilya <zmeev.i.v@edu.mirea.ru>
2026-04-07 16:38:37 +0300

    add Exceptions

---
+++ backend/Flights/Application/Common/Exceptions/ApplicationException.cs
@@ -0,0 +1,28 @@
+﻿namespace Flights.Application.Common.Exceptions;
+
+public class ApplicationException : Exception
+{
+    public string ErrorCode { get; }
+    public Dictionary<string, string[]> Errors { get; }
+
+    public ApplicationException(string message) 
+        : base(message)
+    {
+        ErrorCode = "APPLICATION_ERROR";
+        Errors = new Dictionary<string, string[]>();
+    }
+
+    public ApplicationException(string message, string errorCode) 
+        : base(message)
+    {
+        ErrorCode = errorCode;
+        Errors = new Dictionary<string, string[]>();
+    }
+
+    public ApplicationException(string message, Dictionary<string, string[]> errors) 
+        : base(message)
+    {
+        ErrorCode = "VALIDATION_ERROR";
+        Errors = errors;
+    }
+}
---
+++ backend/Flights/Domain/Exceptions/DomainException.cs
@@ -0,0 +1,28 @@
+﻿namespace Flights.Domain.Exceptions;
+
+public class DomainException : Exception
+{
+    public string ErrorCode { get; }
+    public Dictionary<string, string[]> Errors { get; }
+
+    public DomainException(string message) 
+        : base(message)
+    {
+        ErrorCode = "DOMAIN_ERROR";
+        Errors = new Dictionary<string, string[]>();
+    }
+
+    public DomainException(string message, string errorCode) 
+        : base(message)
+    {
+        ErrorCode = errorCode;
+        Errors = new Dictionary<string, string[]>();
+    }
+
+    public DomainException(string message, Dictionary<string, string[]> errors) 
+        : base(message)
+    {
+        ErrorCode = "VALIDATION_ERROR";
+        Errors = errors;
+    }
+}
