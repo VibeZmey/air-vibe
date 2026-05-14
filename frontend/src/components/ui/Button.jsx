691051d023b8787c06149c9f852dbcd5a10784c7
Zmeev Ilya <zmeev.i.v@edu.mirea.ru>
2026-05-15 02:01:02 +0300

    add button module

---
+++ frontend/src/components/ui/Button.jsx
@@ -0,0 +1,27 @@
+﻿import styles from './Button.module.css';
+
+export function Button({
+  children,
+  variant = 'primary',
+  type = 'button',
+  className = '',
+  disabled = false,
+  fullWidth = false,
+  ...props
+}) {
+  const classes = [
+    styles.button,
+    styles[variant],
+    fullWidth ? styles.fullWidth : '',
+    className,
+  ]
+    .filter(Boolean)
+    .join(' ');
+
+  return (
+    <button type={type} className={classes} disabled={disabled} {...props}>
+      {children}
+    </button>
+  );
+}
+
