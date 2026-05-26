13558be078bc5b34405c384b3d7e611a9a2a0612
Zmeev Ilya <zmeev.i.v@edu.mirea.ru>
2026-05-26 16:48:30 +0300

    add cancel order command

---
+++ backend/Flights/Application/Features/Orders/CancelOrder/CancelOrderHandler.cs
@@ -0,0 +1,29 @@
+﻿using Flights.Domain.Interfaces;
+using MediatR;
+
+namespace Flights.Application.Features.Orders.CancelOrder;
+
+public class CancelOrderHandler : IRequestHandler<CancelOrderCommand, Unit>
+{
+    private readonly IOrderRepository _orderRepo;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CancelOrderHandler(
+        IOrderRepository orderRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _orderRepo = orderRepository;
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task<Unit> Handle(CancelOrderCommand request, CancellationToken cancellationToken)
+    {
+        var order = await _orderRepo.GetByIdAsync(request.OrderId, cancellationToken);
+        if(order is null)
+            throw new ApplicationException("Order not found");
+        //TODO: мб чето придумать пока лень 
+        order.Cancel();
+        await _unitOfWork.SaveAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
